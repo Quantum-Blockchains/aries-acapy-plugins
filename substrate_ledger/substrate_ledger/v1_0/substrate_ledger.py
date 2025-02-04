2f007c0137482e9dbac43b8383a4fdb55282635e
kostiask <kostiaskopec@gmail.com>
2025-02-04 15:22:40 +0100

    test

--- substrate_ledger/substrate_ledger/v1_0/substrate_ledger.py
+++ substrate_ledger/substrate_ledger/v1_0/substrate_ledger.py
@@ -5,7 +5,7 @@ import logging
 LOGGER = logging.getLogger(__name__)
 
 class SubstrateLedger(BaseLedger):
-    def __init__(self, node_url: str, keypair: Keypair = None):
+    def __init__(self):
         LOGGER.info("init substrate ledger")
-        self.substrate = SubstrateInterface(url=node_url)
-        self.keypair = keypair or Keypair.create_from_mnemonic(config.config['agent']['keypair_mnemonic'])
+        self.substrate = SubstrateInterface(url="ws://localhost:9944")
+        # self.keypair = keypair or Keypair.create_from_mnemonic(config.config['agent']['keypair_mnemonic'])
