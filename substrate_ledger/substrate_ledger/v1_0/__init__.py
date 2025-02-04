0a11bb967097541aec8860808e5343c0adaea274
kostiask <kostiaskopec@gmail.com>
2025-02-04 17:30:39 +0100

    update pyproject.toml 2

--- substrate_ledger/substrate_ledger/v1_0/__init__.py
+++ substrate_ledger/substrate_ledger/v1_0/__init__.py
@@ -1,8 +1,9 @@
 from aries_cloudagent.core.plugin_registry import PluginRegistry
 from aries_cloudagent.ledger.base import BaseLedger
 from aries_cloudagent.config.injection_context import InjectionContext
+from .config import get_config
 
 from .substrate_ledger import SubstrateLedger
 
 async def setup(context: InjectionContext):
-    context.injector.bind_instance(BaseLedger, SubstrateLedger(context.settings["substrate_ledger"]["url"]))
+    context.injector.bind_instance(BaseLedger, SubstrateLedger(get_config(context.settings).url))
