70eb40f5fe1957fce7cd67ad8ad1afcbcec2d46d
kostiask <kostiaskopec@gmail.com>
2025-02-04 16:08:00 +0100

    update pyproject.toml 2

--- substrate_ledger/substrate_ledger/v1_0/__init__.py
+++ substrate_ledger/substrate_ledger/v1_0/__init__.py
@@ -6,15 +6,4 @@ from .substrate_ledger import SubstrateLedger
 
 async def setup(context: InjectionContext):
     registry = context.inject(PluginRegistry)
-    registry.register_plugin("substrate_plugin", SubstratePlugin())
-
-class SubstratePlugin:
-    """Substrate Plugin for ACA-Py."""
-
-    def __init__(self):
-        self.name = "substrate_plugin"
-
-    async def register_ledger(self, context: InjectionContext):
-        """Register Substrate ledger."""
-        ledger = SubstrateLedger()
-        context.injector.bind_instance(BaseLedger, ledger)
+    registry.register_ledger(SubstrateLedger())
