ddb25dd42c793118dc51e46d92096efcfd5b7386
kostiask <56359519+kostiask@users.noreply.github.com>
2025-03-06 23:56:55 +0100

    Update registry.py
    Signed-off-by: kostiask <56359519+kostiask@users.noreply.github.com>

--- qmc_registry/qmc_registry/v1_0/registry.py
+++ qmc_registry/qmc_registry/v1_0/registry.py
@@ -6,7 +6,7 @@ from acapy_agent.config.injection_context import InjectionContext # type: ignore
 from acapy_agent.core.profile import Profile # type: ignore
 from acapy_agent.anoncreds.base import BaseAnonCredsResolver, BaseAnonCredsRegistrar # type: ignore
 from acapy_agent.cache.base import BaseCache
-from acapy_agent.anoncreds.models.anoncreds_cred_def import (  # type: ignore
+from acapy_agent.anoncreds.models.credential_definition import (  # type: ignore
     CredDef,
     CredDefResult,
     GetCredDefResult,
